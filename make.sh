b746ac04d979d953d1781fc6b961263a1d3b2b92
Curt Binder <curt.binder@gmail.com>
2020-10-25 21:50:02 -0500

    Updated make script

--- make.sh
+++ make.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
 # The pde.jar file may be buried inside the .app file on Mac OS X.
-PDE="/Applications/Arduino.app/Contents/Java/pde.jar"
-CORE="/Applications/Arduino.app/Contents/Java/arduino-core.jar"
+PDE="/Users/binder/Applications/Arduino.app/Contents/Java/pde.jar"
+CORE="/Users/binder/Applications/Arduino.app/Contents/Java/arduino-core.jar"
 
 javac -target 1.8 -source 1.8 \
   -cp "$CORE:$PDE" \
   -d bin \
   src/CBTool.java
+
+if [ ! -d "tool" ]; then
+  # tool directory doesn't exist, so create it
+  mkdir tool
+fi
 
 cd bin
 zip -x *.DS_Store -r ../tool/cbtool.jar *
