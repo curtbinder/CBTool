c85a5505fd77ebf76fc6d1ff413f3433d44fbbca
Curt Binder <curt.binder@gmail.com>
2020-12-05 15:35:04 -0600

    Updated release script file

--- create-release.sh
+++ create-release.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 
+# variables
+VERSION=`egrep -o '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}' src/CBTool.java`
+ZIPNAME="CBTool-v$VERSION.zip"
+SHANAME="$ZIPNAME.sha256"
+
+printf "Creating release v$VERSION.\n"
+
 # Make the zip file for distribution
+printf "Creating zip file..."
 mkdir CBTool/
 cp -r tool CBTool
-zip -r distribution/CBTool-version.zip CBTool/*
+zip -qr distribution/$ZIPNAME CBTool/*
 rm -rf CBTool/
+printf "OK.\n"
+
+# compute shasum for file
+printf "Creating checksum..."
+cd distribution
+shasum -a 256 $ZIPNAME > $SHANAME
+printf "OK.\n"
+
+printf "Output files stored in distribution/ folder.\n"
