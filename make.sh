0be3348f89dea10528f74f4ea69d06940797885c
Curt Binder <curt.binder@gmail.com>
2021-05-18 20:56:49 -0500

    Fixed up JAR locations for flatpak on popOS

--- make.sh
+++ make.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 # The pde.jar file may be buried inside the .app file on Mac OS X.
-PDE="/Users/binder/Applications/Arduino.app/Contents/Java/pde.jar"
-CORE="/Users/binder/Applications/Arduino.app/Contents/Java/arduino-core.jar"
+#PDE="/Users/binder/Applications/Arduino.app/Contents/Java/pde.jar"
+#CORE="/Users/binder/Applications/Arduino.app/Contents/Java/arduino-core.jar"
+PDE="/home/binder/.local/share/flatpak/app/cc.arduino.arduinoide/current/active/files/Arduino/lib/pde.jar"
+CORE="/home/binder/.local/share/flatpak/app/cc.arduino.arduinoide/current/active/files/Arduino/lib/arduino-core.jar"
 
 javac -target 1.8 -source 1.8 \
   -cp "$CORE:$PDE" \
